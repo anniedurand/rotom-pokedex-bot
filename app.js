c0c2c478c5eeff97bef8b101b29dae5851cc8b99
Annie <annie-d@hotmail.ca>
2016-06-08 22:04:31 +0000

    added a set of functions asking for the user`s game version and to find a matching game entry in the PokéAPI

--- app.js
+++ app.js
@@ -30,7 +30,69 @@ controller.hears(['hello', '^hi$', '^yo$', '^hey$', 'what\'s up'], 'message_rece
 
 // HELP SECTION
 controller.hears('^help$', 'message_received', function(bot, message) {
-  bot.startConversation(message,function(err,convo) {
+  bot.startConversation(message, function(err, convo) {
     convo.say("I am a work in progress, please come back later!");
+  });
+});
+
+
+// WHICH GAME: Finding which game the user is playing for pokedex entry numbers
+
+var userCurrentGame = {};
+
+controller.hears('game', 'message_received', function(bot, message) {   
+  bot.startConversation(message, function(err, convo) {
+    if (!err && !userCurrentGame[message.user]) {
+      convo.ask('Which game are you currently playing?', function(response, convo) {
+        var userAnswer = response.text.toLowerCase();
+        
+        // if 'pokemon' is in the answer, remove it
+        if (userAnswer.indexOf('pokemon') !== -1) {
+          userAnswer = userAnswer.split('pokemon ')[1];
+        } else if (userAnswer.indexOf('pokémon') !== -1) {
+          userAnswer = userAnswer.split('pokémon ')[1];
+        }
+        
+        // REGEXP (for some answers - to avoid having another game/multiple games as a result ('black 2' instead of 'black', etc.))
+        
+        if (userAnswer === 'y') { 
+          userAnswer = /\sy$/;
+        } else if (userAnswer === 'x') {
+          userAnswer = /^x\s/;
+        } else if (userAnswer === 'ruby') {
+          userAnswer = /^ruby\s/;
+        } else if (userAnswer === 'red') {
+          userAnswer = /^red\s/;
+        } else if (userAnswer === 'gold') {
+          userAnswer = /^gold\s/;
+        } else if (userAnswer === 'silver') {
+          userAnswer = /\ssilver$/;
+        } else if (userAnswer === 'white') {
+          userAnswer = /\swhite$/;
+        } else if (userAnswer === 'sapphire') {
+          userAnswer = /^ruby sapphire$/;
+        } else if (userAnswer === 'black') {
+          userAnswer = /^black white$/;
+        }
+        
+        request('https://pokeapi.co/api/v2/version-group/', function (err, result) {
+          if (!err) {
+            var resultObject = JSON.parse(result.body);
+            var results = resultObject.results;
+            
+            results.forEach(function(index) {
+              var currentGameName = index.name.split('-').join(' ');
+              console.log(currentGameName);
+              if (currentGameName.search(userAnswer) !== -1 && currentGameName !== 'colosseum' && currentGameName !== 'xd') {    // ignoring Colosseum and XD
+                console.log('found! here is the url: ' + index.url);
+                userCurrentGame[message.user] = index.name;
+              }
+            });
+          }
+        });
+        convo.say('you said: ' + response.text);  // placeholder
+        convo.next();
+      });
+    }
   });
 });
