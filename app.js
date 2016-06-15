ff038d65c7224fc993d1f48cc99ac5f51ef8bd5e
Annie <annie-d@hotmail.ca>
2016-06-15 22:22:42 +0000

    the whole evolution chain is now displayed before the specific condition

--- app.js
+++ app.js
@@ -299,46 +299,100 @@ controller.on('facebook_postback', function(bot, message) {
 });
 
 function evolutionChain(bot, message, pokemonName, pokemonChainUrl, displayName) {
+  
   request(pokemonChainUrl, function (err, result) {
     if (!err) {
       var evolutionInfos = JSON.parse(result.body);
+      
       bot.startConversation(message, function(err, convo) {
-        var current = pokemonName;
-        console.log(current, 'current')
+        var evoLevelTwoArray = evolutionInfos.chain.evolves_to;
+        var evoLevelThreeArray = evolutionInfos.chain.evolves_to[0].evolves_to;
         
-        if (evolutionInfos.chain.species.name === current && evolutionInfos.chain.evolves_to.length !== 0) {
-          var evoLevelOneArray = evolutionInfos.chain.evolves_to;
-          evoLevelOneArray.forEach(function(pokemon) {
-            var evolved = capitalizeFirst(pokemon.species.name);
-            var levelOne = pokemon.evolution_details[0];  // verify length; is there possibly more than 1?
-            sayEvolutionInfos(convo, levelOne, current, evolved, evolutionInfos, displayName);
-          });
-          convo.say({attachment: mainMenu});
-        } 
-        else if (evolutionInfos.chain.species.name === current && evolutionInfos.chain.evolves_to.length === 0) {
-          convo.say(displayName + ' is at it\'s final evolution stage.');
-          convo.say({attachment: mainMenu});
-        } 
-        else if (evolutionInfos.chain.evolves_to[0].species.name === current && evolutionInfos.chain.evolves_to[0].evolves_to.length !== 0) {
-          var evoLevelTwoArray = evolutionInfos.chain.evolves_to[0].evolves_to;
+        var current = pokemonName;
+        var first = evolutionInfos.chain.species.name;
+        var secondLevel = [];
+        var thirdLevel = [];
+        
+        // pushing all second level pokemon names for a specie in the second level array
+        if (evoLevelTwoArray.length > 0) {
           evoLevelTwoArray.forEach(function(pokemon) {
-            var evolved = capitalizeFirst(pokemon.species.name);
-            var levelTwo = pokemon.evolution_details[0];  // verify length; is there possibly more than 1?
-            sayEvolutionInfos(convo, levelTwo, current, evolved, evolutionInfos, displayName);
+            secondLevel.push(pokemon.species.name);
+          });
+        }
+        
+        // pushing all third level pokemon names for a specie in the third level array
+        if (evoLevelThreeArray.length > 0) {
+          evoLevelThreeArray.forEach(function(pokemon) {
+            thirdLevel.push(pokemon.species.name);
+          });
+        }
+        
+        // sending the right information depending on the evolution chain
+        if (first === current && secondLevel.length > 0) {    // if the current pokemon is the first in the chain and there is a second level
+          if (thirdLevel.length === 0) {
+            convo.say(displayName + ' \u21e8 ' + beautifyWordsArrays(secondLevel));
+          } else {
+            convo.say(displayName + ' \u21e8 ' + beautifyWordsArrays(secondLevel) + ' \u21e8 ' + beautifyWordsArrays(thirdLevel));
+          }
+          
+          evoLevelTwoArray.forEach(function(pokemon) {
+            var evolved = capitalizeFirst(splitJoin(pokemon.species.name));
+            var details = pokemon.evolution_details[0];  // verify length; is there possibly more than 1?
+            sayEvolutionInfos(convo, details, current, evolved, evolutionInfos, displayName);
           });
           convo.say({attachment: mainMenu});
         } 
-        else if (evolutionInfos.chain.evolves_to[0].species.name === current && evolutionInfos.chain.evolves_to[0].evolves_to.length === 0) {
+        
+        else if (first === current && secondLevel.length === 0) {  // if the current pokemon is the first in the chain and there is no second level
+          convo.say(displayName + ' doesn\'t have any known evolution.');
+          convo.say({attachment: mainMenu});
+        } 
+        
+        else if (secondLevel.indexOf(current) !== -1 && thirdLevel.length > 0) {  // if the current pokemon is the second in the chain and there is a third level
+          convo.say(capitalizeFirst(splitJoin(first)) + ' \u21e8 ' + beautifyWordsArrays(secondLevel) + ' \u21e8 ' + beautifyWordsArrays(thirdLevel));
+          
+          evoLevelThreeArray.forEach(function(pokemon) {
+            var evolved = capitalizeFirst(splitJoin(pokemon.species.name));
+            var details = pokemon.evolution_details[0];  // verify length; is there possibly more than 1?
+            sayEvolutionInfos(convo, details, current, evolved, evolutionInfos, displayName);
+          });
+          convo.say({attachment: mainMenu});
+        } 
+        
+        else if (secondLevel.indexOf(current) !== -1 && thirdLevel.length === 0) {  // if the current pokemon is the second in the chain and there is no third level
+          convo.say(capitalizeFirst(splitJoin(first)) + ' \u21e8 ' + beautifyWordsArrays(secondLevel));
           convo.say(displayName + ' is at it\'s final evolution stage.');
           convo.say({attachment: mainMenu});
         } 
-        else if (evolutionInfos.chain.evolves_to[0].evolves_to[0].species.name === current) {
+        
+        else if (thirdLevel.indexOf(current) !== -1) {
+          convo.say(capitalizeFirst(splitJoin(first)) + ' \u21e8 ' + beautifyWordsArrays(secondLevel) + ' \u21e8 ' + beautifyWordsArrays(thirdLevel));
           convo.say(displayName + ' is at it\'s final evolution stage.');
           convo.say({attachment: mainMenu});
         }
       });
     }
   });
+}
+
+function beautifyWordsArrays(array) {
+  var newArray = array.map(function(item) {
+    var words = [];
+    
+    if (item.indexOf('-') !== -1) {
+      words = item.split('-');
+    } else {
+      words.push(item);
+    }
+    
+    var capitalizedWords = words.map(function(word) { 
+      return ' ' + capitalizeFirst(word);
+    });
+    
+    var joinCapitalizedWords = capitalizedWords.join('');
+    return joinCapitalizedWords;
+  });
+  return newArray;
 }
 
 function capitalizeFirst(pokemonName) {
@@ -353,74 +407,74 @@ function reverseSplitJoin(sentence) {
   return sentence.split(' ').join('-');
 }
 
-function trigger(triggerType, evolLevel) {
+function trigger(triggerType, details) {
   if (triggerType === 'level-up') {
     return ' by leveling up:'; 
   } else if (triggerType === 'trade') {
     return ' after being traded with another player:';
   } else if (triggerType === 'use-item') {
-    return ' by being exposed to: ' + splitJoin(evolLevel.item.name) + '.';
+    return ' by being exposed to: ' + splitJoin(details.item.name) + '.';
   } else if (triggerType === 'shed') {
     return ' by shedding (?):'; // ? change this  
   }
 }
 
-function sayEvolutionInfos(convo, evolLevel, current, evolved, evolutionInfos, displayName) {
+function sayEvolutionInfos(convo, details, current, evolved, evolutionInfos, displayName) {
   var conditions = '';
   
-  if (evolLevel.min_level) {
-    conditions += '\n• at level ' + evolLevel.min_level;
+  if (details.min_level) {
+    conditions += '\n• at level ' + details.min_level;
   }
-  if (evolLevel.min_beauty) {
-    conditions += '\n• min. beauty level: ' + evolLevel.min_beauty;  // verify
+  if (details.min_beauty) {
+    conditions += '\n• min. beauty level: ' + details.min_beauty;  // verify
   }
-  if (evolLevel.time_of_day.length > 1) {
-    conditions += '\n• during the ' + evolLevel.time_of_day;
+  if (details.time_of_day.length > 1) {
+    conditions += '\n• during the ' + details.time_of_day;
   }
-  if (evolLevel.gender) {
-    conditions += '\n• it\'s gender must be: ' + evolLevel.gender;  // verify
+  if (details.gender) {
+    conditions += '\n• it\'s gender must be: ' + details.gender;  // verify
   }
-  if (evolLevel.relative_physical_stats) {
-    conditions += '\n• phys. stats: ' + evolLevel.relative_physical_stats; // verify
+  if (details.relative_physical_stats) {
+    conditions += '\n• phys. stats: ' + details.relative_physical_stats; // verify
   }
-  if (evolLevel.needs_overworld_rain) {
+  if (details.needs_overworld_rain) {
     conditions += '\n• while it\'s raining in the overworld';
   }
-  if (evolLevel.turn_upside_down) {
+  if (details.turn_upside_down) {
     conditions += '\n• you have to turn your 3DS upside down';  // verify
   }
-  // if (evolLevel.item) {
-  //   conditions += '\n• using this item: ' + splitJoin(evolLevel.item.name);  // might not be needed if only comes up with item evolution trigger
+  // if (details.item) {
+  //   conditions += '\n• using this item: ' + splitJoin(details.item.name);  // might not be needed if only comes up with item evolution trigger
   // }
-  if (evolLevel.known_move_type) {
-    conditions += '\n• while knowing a ' + splitJoin(evolLevel.known_move_type.name) + '-type move';
+  if (details.known_move_type) {
+    conditions += '\n• while knowing a ' + splitJoin(details.known_move_type.name) + '-type move';
   }
-  if (evolLevel.min_affection) {
-    conditions += '\n• while having at least ' + evolLevel.min_affection + ' affection hearts in Pokémon-Amie';
+  if (details.min_affection) {
+    conditions += '\n• while having at least ' + details.min_affection + ' affection hearts in Pokémon-Amie';
   }
-  if (evolLevel.party_type) {
-    conditions += '\n• party type: ' + evolLevel.party_type;  // verify
+  if (details.party_type) {
+    conditions += '\n• party type: ' + details.party_type;  // verify
   }
-  if (evolLevel.trade_species) {
-    conditions += '\n• trade_species: ' + evolLevel.trade_species; // verify
+  if (details.trade_species) {
+    conditions += '\n• trade_species: ' + details.trade_species; // verify
   }
-  if (evolLevel.party_species) {
-    conditions += '\n• while having a ' + capitalizeFirst(evolLevel.party_species.name) + ' in your party';
+  if (details.party_species) {
+    conditions += '\n• while having a ' + capitalizeFirst(details.party_species.name) + ' in your party';
   }
-  if (evolLevel.min_happiness) {
-    conditions += '\n• min. happiness level: ' + evolLevel.min_happiness;  // verify
+  if (details.min_happiness) {
+    conditions += '\n• min. happiness level: ' + details.min_happiness;  // verify
   }
-  if (evolLevel.held_item) {
-    conditions += '\n• while holding: ' + splitJoin(evolLevel.held_item.name); // verify
+  if (details.held_item) {
+    conditions += '\n• while holding: ' + splitJoin(details.held_item.name); // verify
   }
-  if (evolLevel.known_move) {
-    conditions += '\n• while knowing the move: ' + splitJoin(evolLevel.known_move.name); 
+  if (details.known_move) {
+    conditions += '\n• while knowing the move: ' + capitalizeFirst(splitJoin(details.known_move.name)); 
   }
-  if (evolLevel.location) {
-    conditions += '\n• while being near: ' + splitJoin(evolLevel.location.name);  // see for example eevee to leafeon or glaceon evolution, fix the results.
+  if (details.location) {
+    conditions += '\n• while being near: ' + splitJoin(details.location.name);  // see for example eevee to leafeon or glaceon evolution, fix the results.
   }
   
-  convo.say(displayName + ' evolves to ' + evolved + trigger(evolLevel.trigger.name, evolLevel) + conditions);
+  convo.say(displayName + ' evolves to ' + evolved + trigger(details.trigger.name, details) + conditions);
   
   // find a nice separator (for multiple evolutions like Eevee).... stars ? '\u2606'
 }
